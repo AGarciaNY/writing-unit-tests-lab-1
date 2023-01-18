8316d1a8c542b6f00b9271ca3bba7155d0c6ff2c
AGarciaNY <aldairgarcia360@gmail.com>
2023-01-18 14:49:56 -0500

    feat: created 7 function that where in the test file

--- exercises.js
+++ exercises.js
@@ -1,37 +1,22 @@
 // Question 1
-const isUpperCase = () => {
-
-};
-
+const isUpperCase = (word) => word === word.toUpperCase()
 // Question 2
-const removeVowels = () => {
-
-};
+const removeVowels = (words) => words.map(word => word.replace(/[aeiou]/gi, ""));
 
 // Question 3
-const wordCap = () => {
-
-};
+const wordCap = (s) => s.split(" ").map(word => (/[a-z]/g).test(word[0]) ? word[0].toUpperCase() + word.slice(1,word.length).toLowerCase() :word).join(" ")
 
 // Question 4
-const swapCase = () => {
-
-};
+const swapCase = (word) => [...word].map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join("")
 
 // Question 5
-const staggeredCase = () => {
-
-};
+const staggeredCase = (string) => [...string].reduce((a, b) => (a.word += /[a-z]/gi.test(b) ? (a.cap ? (a.cap = false, b.toUpperCase()) : (a.cap = true, b.toLowerCase())): b , a),{word:"",cap:true}).word;
 
 // Question 6
-const wordLengths = () => {
-
-};
+const wordLengths = (string) => string ? string.split(" ").map(word => `${word} ${word.length}`):[]
 
 // Question 7
-const searchWord = () => {
-
-};
+const searchWord = (word, string) => string.replace(/[.!?,]/ig, "").split(" ").filter(w => w.toLowerCase() === word.toLowerCase()).length
 
 
 // The code below ensures that this file can talk to our test file.
--- exercises.test.js
+++ exercises.test.js
@@ -12,51 +12,51 @@ test('Question 0: add', () => {
 
 
 test("Question 1: isUpperCase", () => {
-  expect(isUpperCase('t')).toEqual(false);
-  expect(isUpperCase('T')).toEqual(true);
-  expect(isUpperCase('Four Score')).toEqual(false);
-  expect(isUpperCase('FOUR SCORE')).toEqual(true);
-  expect(isUpperCase('4SCORE!')).toEqual(true);
-  expect(isUpperCase('')).toEqual(true);
+  expect(exercises.isUpperCase('t')).toEqual(false);
+  expect(exercises.isUpperCase('T')).toEqual(true);
+  expect(exercises.isUpperCase('Four Score')).toEqual(false);
+  expect(exercises.isUpperCase('FOUR SCORE')).toEqual(true);
+  expect(exercises.isUpperCase('4SCORE!')).toEqual(true);
+  expect(exercises.isUpperCase('')).toEqual(true);
 });
 
 test("Question 2: removeVowels", () => {
-  expect(removeVowels(['abcdefghijklmnopqrstuvwxyz'])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
-  expect(removeVowels(['green', 'YELLOW', 'black', 'white'])).toEqual(["grn", "YLLW", "blck", "wht"]);
-  expect(removeVowels(['ABC', 'AEIOU', 'XYZ'])).toEqual(["BC", "", "XYZ"]);
+  expect(exercises.removeVowels(['abcdefghijklmnopqrstuvwxyz'])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
+  expect(exercises.removeVowels(['green', 'YELLOW', 'black', 'white'])).toEqual(["grn", "YLLW", "blck", "wht"]);
+  expect(exercises.removeVowels(['ABC', 'AEIOU', 'XYZ'])).toEqual(["BC", "", "XYZ"]);
 })
 
 test("Question 3: wordCap", () => {
-  expect(wordCap('four score and seven')).toEqual("Four Score And Seven");
-  expect(wordCap('the javaScript language')).toEqual("The Javascript Language");
-  expect(wordCap('this is a "quoted" word')).toEqual('This Is A "quoted" Word');
+  expect(exercises.wordCap('four score and seven')).toEqual("Four Score And Seven");
+  expect(exercises.wordCap('the javaScript language')).toEqual("The Javascript Language");
+  expect(exercises.wordCap('this is a "quoted" word')).toEqual('This Is A "quoted" Word');
 })
 
 test("Question 4: swapCase", () => {
-  expect(swapCase('CamelCase')).toEqual("cAMELcASE");
-  expect(swapCase('Tonight on XYZ-TV')).toEqual("tONIGHT ON xyz-tv");
+  expect(exercises.swapCase('CamelCase')).toEqual("cAMELcASE");
+  expect(exercises.swapCase('Tonight on XYZ-TV')).toEqual("tONIGHT ON xyz-tv");
 })
 
 test("Question 5: staggeredCase", () => {
-  expect(staggeredCase('I Love Launch School!')).toEqual("I lOvE lAuNcH sChOoL!");
-  expect(staggeredCase('ALL CAPS')).toEqual("AlL cApS");
-  expect(staggeredCase('ignore 77 the 444 numbers')).toEqual("IgNoRe 77 ThE 444 nUmBeRs");
+  expect(exercises.staggeredCase('I Love Launch School!')).toEqual("I lOvE lAuNcH sChOoL!");
+  expect(exercises.staggeredCase('ALL CAPS')).toEqual("AlL cApS");
+  expect(exercises.staggeredCase('ignore 77 the 444 numbers')).toEqual("IgNoRe 77 ThE 444 nUmBeRs");
 })
 
 test("Question 6: isUpperCase", () => {
-  expect(wordLengths('cow sheep chicken')).toEqual(["cow 3", "sheep 5", "chicken 7"])
-  expect(wordLengths('baseball hot dogs and apple pie')).toEqual(["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"])
-  expect(wordLengths("It ain't easy, is it?")).toEqual(["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"])
-  expect(wordLengths('Supercalifragilisticexpialidocious')).toEqual(["Supercalifragilisticexpialidocious 34"])
-  expect(wordLengths('')).toEqual([])
-  expect(wordLengths()).toEqual([])
+  expect(exercises.wordLengths('cow sheep chicken')).toEqual(["cow 3", "sheep 5", "chicken 7"])
+  expect(exercises.wordLengths('baseball hot dogs and apple pie')).toEqual(["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"])
+  expect(exercises.wordLengths("It ain't easy, is it?")).toEqual(["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"])
+  expect(exercises.wordLengths('Supercalifragilisticexpialidocious')).toEqual(["Supercalifragilisticexpialidocious 34"])
+  expect(exercises.wordLengths('')).toEqual([])
+  expect(exercises.wordLengths()).toEqual([])
 })
 
 test("Question 7: isUpperCase", () => {
   let text = 'D Smoke is humble. The Inglewood native exudes an aura of maturation, needed for his quick ascension into popular culture as the first winner of Rhythm + Flow, Netflix’s hip-hop reality competition centered on the discovery of hip-hop’s next star. His signature authenticity shone throughout the 10-episode series and international audiences were drawn to his charisma as he proudly rapped about his lived experiences as a young black man in Inglewood.';
 
-  expect(searchWord('his', text)).toEqual(4)
-  expect(searchWord('hip-hop', text)).toEqual(1) // (because "hip-hop's" !== "hip-hop")
-  expect(searchWord('flow', text)).toEqual(1)
-  expect(searchWord('is', text)).toEqual(1)
+  expect(exercises.searchWord('his', text)).toEqual(4)
+  expect(exercises.searchWord('hip-hop', text)).toEqual(1) // (because "hip-hop's" !== "hip-hop")
+  expect(exercises.searchWord('flow', text)).toEqual(1)
+  expect(exercises.searchWord('is', text)).toEqual(1)
 })
